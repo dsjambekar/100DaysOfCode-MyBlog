caff6950bce0b6c2011813f9a7c77bccaee98b70
dsjambekar <dsjambekar@gmail.com>
2022-04-22 12:21:10 +0530

    Day4

--- MyBlog/MyBlog/MyBlogTest/BlogControllerTest.cs
+++ MyBlog/MyBlog/MyBlogTest/BlogControllerTest.cs
@@ -1,4 +1,5 @@
-﻿using Moq;
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
 using MyBlog.Controllers;
 using MyBlog.Models;
 using MyBlog.Services;
@@ -13,10 +14,11 @@ namespace MyBlogTest
 {
     public class BlogControllerTest
     {
-        private BlogController _controllerUderTest;
+        private BlogController _controllerUnderTest;
+        private Mock<IBlogsService> _moqService;
 
         [Fact]
-        public void Get_returnsAllPosts()
+        public async void Get_returnsAllPosts()
         {
             //Arrange
             var post = new Post()
@@ -30,18 +32,101 @@ namespace MyBlogTest
 
             var posts = new List<Post>() { post };
 
-            Mock<IBlogsService> moqService = new Mock<IBlogsService>();
-            moqService.Setup(x => x.GetAsync())
+            _moqService = new Mock<IBlogsService>();
+            _moqService.Setup(x => x.GetAsync())
                 .ReturnsAsync(posts);
 
-            _controllerUderTest = new BlogController(moqService.Object);
+            _controllerUnderTest = new BlogController(_moqService.Object);
 
             //Act
-            var value = _controllerUderTest.Get();
+            var value = await _controllerUnderTest.Get();
 
             //Assert
             Assert.Same(value, posts);
+        }
 
+        [Fact]
+        public async void get_ReturnsPostForGivenId()
+        {
+            //Arrange
+            string postId = "12345678901234567890";
+            var post = new Post()
+            {
+                Id = postId,
+                Title = "some test title",
+                Author = "test author",
+                Body = "random test body",
+                Category = "test"
+            };
+
+            _moqService = new Mock<IBlogsService>();
+            _moqService.Setup(x => x.GetAsync(postId))
+                .ReturnsAsync(post);
+
+            _controllerUnderTest = new BlogController(_moqService.Object);
+
+            //Act
+            var result = await _controllerUnderTest.Get(postId);
+
+            //Assert
+            Assert.Same(post, result.Value);
+        }
+
+        [Fact]
+        public async void get_ReturnsNotFoundErrorForUnkownId()
+        {
+            //Arrange
+            string postId = "12345678901234567890";
+            var post = new Post()
+            {
+                Id = postId,
+                Title = "some test title",
+                Author = "test author",
+                Body = "random test body",
+                Category = "test"
+            };
+
+            _moqService = new Mock<IBlogsService>();
+            _moqService.Setup(x => x.GetAsync(postId))
+                .ReturnsAsync(post);
+
+            _controllerUnderTest = new BlogController(_moqService.Object);
+
+            //Act
+            var result = await _controllerUnderTest.Get("0987654321234567890");
+
+            //Assert
+            Assert.IsType<NotFoundResult>(result.Result);
+
+        }
+
+        [Fact]
+        public async void post_returnsCreatedResponse()
+        {
+            //Arrange
+            string postId = "12345678901234567890";
+            var post = new Post()
+            {
+                Id = postId,
+                Title = "some test title",
+                Author = "test author",
+                Body = "random test body",
+                Category = "test"
+            };
+
+            _moqService = new Mock<IBlogsService>();
+            _moqService.Setup(x => x.CreateAsync(post))
+                .Verifiable();
+
+            _controllerUnderTest = new BlogController(_moqService.Object);
+
+            //Act
+            var result = await _controllerUnderTest.Post(post);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.IsType<CreatedAtActionResult>(result);
+            //Assert.Equal(10, result.RouteValues["id"]);
 
         }
     }
